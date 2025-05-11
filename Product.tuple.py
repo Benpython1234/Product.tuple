9d7aff246a0192d194e43ec0200341c938be1de6
Benpython1234 <benjoash15@gmail.com>
2025-05-11 19:19:33 +1000

    Product.tuple.py

--- Product.tuple.py
+++ Product.tuple.py
@@ -1,7 +1,14 @@
-NUMBERS = (1,2,3,4,5)
-PRODUCT = 1
-for num in NUMBERS:
-    PRODUCT = num* PRODUCT
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
 
-print(f"The product of the tuple is: {PRODUCT}")
+def tuple_product(tup):
+    product = 1
+    for num in tup:
+        product *= num
+    return product
 
+print("Tuple 1:", tup1)
+print("Product of Tuple 1:", tuple_product(tup1))
+
+print("\nTuple 2:", tup2)
+print("Product of Tuple 2:", tuple_product(tup2))
